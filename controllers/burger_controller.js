b232f522a2efd0b4ce8973dafc7ca809f33dd0a4
Justin Taylor <kiradez211@gmail.com>
2020-06-12 00:31:34 -0400

    Updated controller

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -7,7 +7,7 @@ var router = express.Router();
 router.get("/", function (req, res){
     burger.selectAll(function(data){
         var hbsObject = {
-            burger: data
+            burgers: data
         };
         console.log(hbsObject);
         res.render("index", hbsObject);
